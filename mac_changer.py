5dd45cafdca179a0649f18719bcba8c477f77fa4
Shuga8 <lotocharles8@gmail.com>
2024-01-22 18:40:44 -0500

    parser fo passing argument
    using arguments only no inputs

--- mac_changer.py
+++ mac_changer.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = raw_input("Interface name >> ")
+parser = optparse.OptionParser()
 
-new_mac_address = raw_input("New MAC address >> ")
+parser.add_option("-i", "--interface", dest="interface",help="interface name to change it's mac address")
+
+parser.add_option("-m", "--mac", dest="new_mac_address",help="new MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+
+new_mac_address = options.new_mac_address
 
 print("[+] Changing mac address for " + interface + " to " + new_mac_address)
 
