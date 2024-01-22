7161c384331670b7a1335c60774c572d479a8c66
Shuga8 <lotocharles8@gmail.com>
2024-01-21 20:59:17 -0500

    completed

--- mac_changer.py
+++ mac_changer.py
@@ -1,3 +1,7 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
 import subprocess
+
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 22:33:75:28:99:12", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
